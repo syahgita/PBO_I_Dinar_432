476790df188d3061c9f8dd83cc4cffed9b03f570
syahgita <dinargita19@gmail.com>
2025-03-16 21:32:07 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,10 @@
+public class Hewan {
+    String nama, jenis, suara;
+
+    void tampilkanInfo(){
+        System.out.println("nama  :" + nama);
+        System.out.println("jenis :" + jenis);
+        System.out.println("suara :" + suara + "\n");
+
+    }
+}
